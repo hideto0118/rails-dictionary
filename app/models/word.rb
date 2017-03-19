bcfea12e28b96017fcfb6adefb168062ca3ef9d2
Hideto Yasunori <hideto0118yasunori@gmail.com>
2017-03-18 23:54:28 -0700

    Modify seach function

--- app/models/word.rb
+++ app/models/word.rb
@@ -2,7 +2,7 @@ class Word < ActiveRecord::Base
 
   def self.search(search)
     where("sentence LIKE ?", "%#{search}%")
-    where("description LIKE ?", "%#{search}%")
+    # where("description LIKE ?", "%#{search}%")
   end
 
 end
